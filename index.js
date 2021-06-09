18554077a46625a0d8b5b8ca7c315006796aef6c
santiagoOjeda <santiago.ojeda.dm@gmail.com>
2021-06-09 18:01:07 -0300

    test

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 express()
-  /*   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index')) */
-  .get("/test", function (req, res) {
+  .get("/", function (req, res) {
     res.send("WORKK!!!!");
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
