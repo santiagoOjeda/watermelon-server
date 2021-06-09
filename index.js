8e4da58bae6b4c7dad98e84b9d715384dc635e4c
santiagoOjeda <santiago.ojeda.dm@gmail.com>
2021-06-09 17:30:38 -0300

    add express

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-
 const app = express();
+const path = require("path");
+const port = 3000;
 
-app.get("/test", function (req, res) {
-  res.send("Hello");
+app.get("/", function (req, res) {
+  // res.sendFile(path.join(__dirname + "/index.html"));
+  res.send("WORK!!!!!");
 });
 
-app.listen(4000);
-
-console.log("server running!!!!!");
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
