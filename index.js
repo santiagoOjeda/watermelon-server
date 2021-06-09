1959218f17ae383d4f20785a89fb7074fa88c608
santiagoOjeda <santiago.ojeda.dm@gmail.com>
2021-06-09 17:00:05 -0300

    add express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("works!!!!!");
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello");
+});
+
+app.listen(3000);
+
+console.log("server running!!!!!");
