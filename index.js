8b7c4d323add61240d2a3fdd9ff0bfc1b50be0ad
santiagoOjeda <santiago.ojeda.dm@gmail.com>
2021-06-09 17:35:35 -0300

    work please

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get("/", function (req, res) {
+app.get("/test", function (req, res) {
   // res.sendFile(path.join(__dirname + "/index.html"));
   res.send("WORK!!!!!");
 });
