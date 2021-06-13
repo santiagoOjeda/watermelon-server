0f514ac0b878225422bfe01fa631be60c30aa855
santiagoDanielOjeda <danielsantiago.ojeda@mercadolibre.com>
2021-06-13 10:43:26 -0300

    add cors config

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const posts = require('./routes/post');
+var cors = require('cors');
 const PORT = process.env.PORT || 5000;
+
+var corsOptions = {
+  origin: 'http://watermelon-server.herokuapp.com',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 app.use('/post', posts);
 
