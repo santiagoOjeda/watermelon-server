11129c8547495426886f925f151b2b495250a4f4
santiagoOjeda <santiago.ojeda.dm@gmail.com>
2021-06-16 08:01:55 -0300

    test 1

--- routes/post.js
+++ routes/post.js
@@ -14,6 +14,12 @@ router.get("/all", async function (req, res) {
 
 router.post("/save", async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
   const postList = await postService.save(req.body);
   res.send(postList);
 });
