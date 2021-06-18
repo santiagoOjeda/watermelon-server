f787fc5f2774ada74e873e804cdbc6d00288985b
santiagoOjeda <santiago.ojeda.dm@gmail.com>
2021-06-18 08:10:33 -0300

    test new endpoint

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ const PORT = process.env.PORT || 5000;
 const posts = require("./routes/post");
 require("./connection");
 
-const corsOptions = {
-  origin: "http://watermelon-server.herokuapp.com",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+app.use(cors());
 
-app.use(cors(corsOptions));
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/post", posts);
+
+app.use("/save", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  const postList = postService.save(req.body);
+  res.send(postList);
+});
 
 app.get("/", function (req, res) {
   res.send("WORKK !!!");
