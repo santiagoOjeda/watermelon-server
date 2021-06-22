bbe9dc93539f8004b9f40cea0e233749e18a7bb9
santiagoDanielOjeda <danielsantiago.ojeda@mercadolibre.com>
2021-06-22 14:05:18 -0300

    add new route, save-image

--- routes/post.js
+++ routes/post.js
@@ -1,23 +1,27 @@
-const express = require("express");
+const express = require('express');
+const app = express();
 const router = express.Router();
-const postService = require("../services/post");
-const cors = require("cors");
-express().use(cors());
+const postService = require('../services/post');
+const cors = require('cors');
+//res.setHeader('Access-Control-Allow-Origin', '*');
+app.use(cors());
 
-router.get("/", function (req, res) {
+router.get('/', function (req, res) {
   res.send(postService.save());
 });
 
-router.get("/all", async function (req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+router.get('/all', async function (req, res) {
   const postList = await postService.list();
   res.json(postList);
 });
 
-router.post("/save", async (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
+router.post('/save', async (req, res) => {
   const postList = await postService.save(req.body);
   res.send(postList);
+});
+
+router.post('/save-image', async (req, res) => {
+  res.send('Image saved');
 });
 
 module.exports = router;
