e714594af92f8fdc7e8a169ffc08d09f1fa62a7b
santiagoOjeda <santiago.ojeda.dm@gmail.com>
2021-06-23 08:51:01 -0300

    change routes names

--- routes/post.js
+++ routes/post.js
@@ -1,32 +1,35 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const router = express.Router();
-const postService = require('../services/post');
-const multer = require('multer');
-const cors = require('cors');
+const postService = require("../services/post");
+const multer = require("multer");
+const cors = require("cors");
+
 // middlewares
 app.use(cors());
 
 app.use(
-  multer({ dest: path.join(__dirname, 'public/uploads') }).single('image')
+  multer({ dest: path.join(__dirname, "../public/uploads") }).single("image")
 );
 
-// reoutes
-router.get('/', function (req, res) {
+// routes
+router.get("/", function (req, res) {
   res.send(postService.save());
 });
 
-router.get('/all', async function (req, res) {
+router.get("/all", async function (req, res) {
   const postList = await postService.list();
   res.json(postList);
 });
 
-router.post('/save', async (req, res) => {
+router.post("/new", async (req, res) => {
   const postList = await postService.save(req.body);
   res.send(postList);
 });
-router.post('/save-image', async (req, res) => {
+
+router.post("/save-image", (req, res) => {
+  console.log(req);
   res.send(req.file);
 });
 
