ff6c20fa979371933f3026553b736ca16bdb6a32
santiagoOjeda <santiago.ojeda.dm@gmail.com>
2021-07-04 10:11:49 -0300

    issue

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-import express from "express";
-import cors from "cors";
-import "./connection";
-import bodyParser from "body-parser";
-import posts from "./src/routes/post";
-
-const PORT = process.env.PORT || 5050;
-
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const PORT = process.env.PORT || 5050;
+const posts = require("./src/routes/post");
+require("./connection");
+
 app.use(cors());
 
+//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use("/post", posts);
 
 app.get("/", function (req, res) {
   res.send("WORKK !!!");
 });
-
-app.use("/post", posts);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
