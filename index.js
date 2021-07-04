56ca0b43bf6cc9d301f9406f7823d5dd700a9be6
santiagoOjeda <santiago.ojeda.dm@gmail.com>
2021-07-04 10:07:51 -0300

    ...

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-import app from "./app";
+import express from "express";
 import cors from "cors";
+import "./connection";
+import bodyParser from "body-parser";
+import posts from "./src/routes/post";
 
 const PORT = process.env.PORT || 5050;
 
+const app = express();
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+  res.send("WORKK !!!");
+});
+
+app.use("/post", posts);
+
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
