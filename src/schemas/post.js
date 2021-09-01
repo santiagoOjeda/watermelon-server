a48bb32dbbc9faa5a49c72155cc99020e920370c
santiagoOjeda <santiago.ojeda.dm@gmail.com>
2021-09-01 11:54:01 -0300

    add default to date schema

--- src/schemas/post.js
+++ src/schemas/post.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 const PostSchema = new Schema({
   title: { type: String },
   paragraph: { type: String },
-  date: { type: String },
+  date: { type: Date, default: Date.now },
   author: { type: String },
   bgColor: { type: String },
   img: { type: Buffer },
