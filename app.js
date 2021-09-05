7319a67938fa047ed0ae6c996f1590e258f51766
santiagoOjeda <santiago.ojeda.dm@gmail.com>
2021-09-05 08:47:41 -0300

    test fix

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-import express from 'express';
-import cors from 'cors';
-import './connection';
-import bodyParser from 'body-parser';
-import posts from './src/routes/post';
+import express from "express";
+import cors from "cors";
+import "./connection";
+import bodyParser from "body-parser";
+import posts from "./src/routes/post";
 
 const app = express();
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('WORKK !!!');
+app.get("/", function (req, res) {
+  res.send("WORKK !!!");
 });
 
-app.use('/post', posts);
+app.use("/post", posts);
 
 export default app;
